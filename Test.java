abd354ec3ce2f1ea4bac467463f7dfd72db0a81b
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2024-09-02 12:21:39 -0500

    Initial commits for test files

---
+++ Test.java
@@ -0,0 +1,10 @@
+import java.time.*;
+public class Test {
+	public static void main(String args[]) {
+		LocalDateTime dt = LocalDateTime.of(2020, 12, 31, 8, 30);
+		for (int i = 0; i < 5; i++) {
+			System.out.println(dt);
+			dt = dt.plusMinutes(30);			
+		}
+	}
+}
